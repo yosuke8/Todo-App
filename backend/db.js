f9a651be3a929541ef79f8f4ea6d03218fe6b9a7
YOSUKE SAKURAI <nicorasgage2892@gmail.com>
2025-02-17 13:42:09 +0900

    Switch to better-sqlite3 for Render

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require("better-sqlite3").verbose();
 
 // `database.db` というファイルを作成（なければ自動生成）
 const db = new sqlite3.Database("./database.db", (err) => {
